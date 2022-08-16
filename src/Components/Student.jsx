c9316a9d79e89fdf64d071340cc828f1b973e916
Preeti-p1 <preetchauhan211999@gmail.com>
2022-08-16 17:37:29 +0530

    first commit

---
+++ src/Components/Student.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './student.css'
+
+const Student = () => {
+  return (
+    <>
+       <h1>Student's Record</h1>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Roll no</th>
+                <th>Course</th>
+            </tr>
+            <tr>
+                <td>PITAMBAR BHADRA</td>
+                <td>001</td>
+                <td>Full Stack Developer</td>
+            </tr>
+            <tr>
+                <td>PUSHPENDRA SINGH</td>
+                <td>002</td>
+                <td>Full Stack Developer</td>
+            </tr>
+            <tr>
+                <td>RUPAM DAS</td>
+                <td>003</td>
+                <td>Full Stack Developer</td>
+            </tr>
+            <tr>
+                <td>RITIKA JOSHI</td>
+                <td>004</td>
+                <td>Full Stack Developer</td>
+            </tr>
+            <tr>
+                <td>ABHINASH PARIDA</td>
+                <td>005</td>
+                <td>Full Stack Developer</td>
+            </tr>
+        </table>
+    </>
+  )
+}
+
+export default Student
